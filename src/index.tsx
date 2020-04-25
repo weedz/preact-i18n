045e08d574a08dfe7e135a58cc4d80c51b1cb8d7
Linus Björklund <weedzcokie@gmail.com>
2020-04-25 19:32:04 +0200

    WIP: Types

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import { h, Component } from "preact";
 
 let language = "en";
-let listeners = [];
+let listeners: Function[] = [];
 
-export function subscribe(listener) {
+export function subscribe(listener: Function) {
     listeners.push(listener);
     return () => { unsubscribe(listener); };
 }
-function unsubscribe(listenerRemove) {
-    listeners = listeners.filter(listener => listener !== listenerRemove);
-    listenerRemove = null;
+function unsubscribe(cb: Function) {
+    listeners.splice(listeners.indexOf(cb)>>>0, 1);
 }
 
-export function mergeStrings(strings = {}, newStrings) {
+type Strings = {
+    [key: string]: string|Function
+};
+
+export function mergeStrings(strings: Strings = {}, newStrings: Strings[]) {
     for (let texts of newStrings) {
         if (typeof texts.default === "object") {
             Object.assign(strings, texts.default);
@@ -23,7 +26,7 @@ export function mergeStrings(strings = {}, newStrings) {
     return strings;
 }
 
-export function changeLanguage(newLanguage) {
+export function changeLanguage(newLanguage: string) {
     language = newLanguage;
     for (let listener of listeners) {
         listener(newLanguage);
@@ -34,23 +37,38 @@ export function currentLanguage() {
 }
 
 
-export function localize(locales) {
+export function localize(locales: {[language: string]: Function}): {
+    activeLocale: Strings,
+    languages: typeof locales
+} {
     return {
         activeLocale: {},
         languages: locales
     };
 }
 
-export default function connectLanguage(Child, locales) {
-    return class Wrapper extends Component {
+export type LanguageProps = {
+    string: Strings
+}
+
+// export function connectLanguage(Child: VNode<typeof locales>, locales: ReturnType<typeof localize>) {
+export function connectLanguage(
+    Child: any,
+    locales: ReturnType<typeof localize>
+) {
+    return class Wrapper extends Component<{}, {languageLoaded: boolean}> {
+        unsubscribe: Function;
         constructor() {
             super();
             this.state = {
                 languageLoaded: false
             };
+            this.unsubscribe = subscribe((newLanguage: string) => {
+                this.setLanguage(newLanguage);
+            });
         }
         
-        async setLanguage(language) {
+        async setLanguage(language: string) {
             if (!locales.languages[language]) {
                 // TODO: set/get default language
                 language = "en";
@@ -58,17 +76,13 @@ export default function connectLanguage(Child, locales) {
             mergeStrings(locales.activeLocale, await Promise.all(locales.languages[language]()));
             this.setState({languageLoaded: true});
         }
-        string(id, ...params) {
-            if (typeof locales.activeLocale[id] === "function") {
-                return locales.activeLocale[id](...params);
+        string(id: string, ...params: any) {
+            const activeLocale = locales.activeLocale[id];
+            if (typeof activeLocale === "function") {
+                return activeLocale(...params);
             }
             return locales.activeLocale[id];
         }
-        componentDidMount() {
-            this.unsubscribe = subscribe(newLanguage => {
-                this.setLanguage(newLanguage);
-            });
-        };
         componentWillUnmount() {
             if (this.unsubscribe) {
                 this.unsubscribe();
