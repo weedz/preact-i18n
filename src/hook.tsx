f3b3c31166804d7e073d890d5c53a8e202b13f48
Linus Björklund <weedzcokie@gmail.com>
2023-05-19 10:07:37 +0200

    Add dependency array  to `useEffect` call

--- src/hook.tsx
+++ src/hook.tsx
@@ -3,17 +3,17 @@ import { subscribe, unsubscribe } from "./connect.js";
 import { strings } from "./utils.js";
 
 export function useLanguage() {
-    const [currentState, setState] = useState(false);
+    const [_s, setState] = useState(false);
 
     useEffect(() => {
         function updateStrings(_newStrings: typeof strings) {
-            setState(!currentState);
+            setState(currentState => !currentState);
         }
         subscribe(updateStrings);
         return () => {
             unsubscribe(updateStrings);
         }
-    });
+    }, []);
 
     return strings as Readonly<typeof strings>;
 }
